673e24c1f9d9b193a055ac8ed0214af90a3b31ab
pikulkaew <63611089@kmitl.ac.th>
2022-06-20 16:40:59 +0700

    change worker2

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -1,3 +1,14 @@
-bind = "0.0.0.0:8080"
-workers = 4
-timeout = 300
+# bind = "0.0.0.0:8080"
+# workers = 4
+# timeout = 300
+
+NUM_WORKERS=3
+TIMEOUT=120
+
+exec gunicorn ${DJANGO_WSGI_MODULE}:application \
+--name $NAME \
+--workers $NUM_WORKERS \
+--timeout $TIMEOUT \
+--log-level=debug \
+--bind=127.0.0.1:9000 \
+--pid=$PIDFILE
